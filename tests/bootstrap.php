5a5f3d9767e121877f5ab2bf9cff02770ad02cf9
Derrick Hammer <derrick@derrickhammer.com>
2017-05-26 23:57:13 -0400

    *Add missing boostrap.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package Wp_Criticalcss
+ */
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+// Initize composer
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require dirname( dirname( __FILE__ ) ) . '/wp-criticalcss.php';
+	remove_action( 'plugins_loaded', 'wp_criticalcss_init' );
+}
+
+function wp_criticalcss_test_autoloader( $class_name ) {
+	$file      = 'class-' . str_replace( '_', '-', strtolower( $class_name ) ) . '.php';
+	$base_path = trailingslashit( __DIR__ );
+
+	$paths = array(
+		$base_path . $file,
+	);
+	foreach ( $paths as $path ) {
+
+		if ( is_readable( $path ) ) {
+			include_once( $path );
+
+			return;
+		}
+	}
+}
+
+spl_autoload_register( 'wp_criticalcss_test_autoloader' );
+
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
