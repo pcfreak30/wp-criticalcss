05567f22640dc4a246566eeb935a119436f41de9
Derrick Hammer <derrick@derrickhammer.com>
2017-05-27 20:15:28 -0400

    *Use require instead of config for testcase class

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -47,3 +47,4 @@ tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
+require_once __DIR__ . '/wp-criticalcss-testcase.php';
