075d74a7a0d9ac4de04fe30268ebff852ae3d7f3
Derrick Hammer <derrick@derrickhammer.com>
2017-05-30 23:24:09 -0400

    *Use runTestsInSeparateProcesses not runInSeparateProcess *Fix bad usages of assertFalse *setup_integrations call not needed

--- tests/integrations/test-class-wp-criticalcss-integration-wp-rocket.php
+++ tests/integrations/test-class-wp-criticalcss-integration-wp-rocket.php
@@ -2,16 +2,14 @@
 
 /**
  * @group wp-rocket
- * @runInSeparateProcess
+ * @runTestsInSeparateProcesses
  */
 class Test_WP_CriticalCSS_Integration_WP_Rocket extends WP_CriticalCSS_TestCase {
 
 	public function test_enable() {
 		global $wp;
 		WPCCSS()->init();
-		WPCCSS()->setup_integrations( true );
 		do_action_ref_array( 'wp', array( &$wp ) );
-
 		$this->assertEquals( 10, has_action( 'after_rocket_clean_domain', array(
 			WPCCSS(),
 			'reset_web_check_transients',
@@ -20,19 +18,18 @@ class Test_WP_CriticalCSS_Integration_WP_Rocket extends WP_CriticalCSS_TestCase 
 
 	public function test_disable() {
 		global $wp;
-		$this->assertFalse( 10, has_action( 'after_rocket_clean_domain', array(
+		$this->assertFalse( has_action( 'after_rocket_clean_domain', array(
 			WPCCSS(),
 			'reset_web_check_transients',
 		) ) );
 		WPCCSS()->init();
-		WPCCSS()->setup_integrations( true );
 		do_action_ref_array( 'wp', array( &$wp ) );
 		$this->assertEquals( 10, has_action( 'after_rocket_clean_domain', array(
 			WPCCSS(),
 			'reset_web_check_transients',
 		) ) );
 		WPCCSS()->disable_integrations();
-		$this->assertFalse( 10, has_action( 'after_rocket_clean_domain', array(
+		$this->assertFalse( has_action( 'after_rocket_clean_domain', array(
 			WPCCSS(),
 			'reset_web_check_transients',
 		) ) );
