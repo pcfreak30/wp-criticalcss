6c601fd122cbf98b794b9f7b08fd9c9cd16ae5c8
Derrick Hammer <derrick@derrickhammer.com>
2017-07-10 19:13:31 -0400

    *Cache code has been refactored to a separate package

--- lib/WP/CriticalCSS/Cache/Manager.php
+++ lib/WP/CriticalCSS/Cache/Manager.php
@@ -2,6 +2,7 @@
 
 namespace WP\CriticalCSS\Cache;
 
+use pcfreak30\WordPress\Cache\Store;
 use WP\CriticalCSS;
 
 /**
@@ -11,6 +12,12 @@ use WP\CriticalCSS;
  * @SuppressWarnings(PHPMD.ExcessiveClassComplexity)
  */
 class Manager extends CriticalCSS\ComponentAbstract {
+
+	/**
+	 * @var \pcfreak30\WordPress\Cache\Store
+	 */
+	private $store;
+
 	/**
 	 *
 	 */
@@ -42,88 +49,15 @@ class Manager extends CriticalCSS\ComponentAbstract {
 				]
 			);
 		}
+		$this->store = new Store( CriticalCSS::TRANSIENT_PREFIX, apply_filters( 'wp_criticalcss_cache_expire_period', absint( $this->settings['web_check_interval'] ) ), apply_filters( 'rocket_async_css_max_branch_length', 50 ) );
 	}
 
 	public function delete_cache_branch( $path = [] ) {
-		$result = false;
-		if ( is_array( $path ) ) {
-			if ( ! empty( $path ) ) {
-				$path = $this->set_path_defaults( $path );
-				$path = CriticalCSS::TRANSIENT_PREFIX . implode( '_', $path ) . '_';
-			} else {
-				$path = CriticalCSS::TRANSIENT_PREFIX;
-			}
-		}
-		$counter_transient = "{$path}cache_count";
-		$counter           = $this->get_transient( $counter_transient );
-
-		if ( is_null( $counter ) || false === $counter ) {
-			return $this->delete_cache_leaf( rtrim( $path, '_' ) );
-		}
-		for ( $i = 1; $i <= $counter; $i ++ ) {
-			$transient_name = "{$path}cache_{$i}";
-			$cache          = $this->get_transient( "{$path}cache_{$i}" );
-			if ( ! empty( $cache ) ) {
-				$branch_result = false;
-				foreach ( $cache as $sub_branch ) {
-					$branch_result = $this->delete_cache_branch( "{$sub_branch}_" );
-				}
-				$result = $branch_result && $this->delete_cache_leaf( $transient_name );
-			}
-		}
-		$this->delete_transient( $counter_transient );
-
-		return $result;
-	}
-
-	protected function set_path_defaults( $path ) {
-		$defaults = [ 'cache' ];
-		if ( is_multisite() ) {
-			$defaults[] = 'blog-' . get_current_blog_id();
-		}
-		$path = array_merge( $defaults, $path );
-		$path = array_unique( $path );
-
-		return $path;
-	}
-
-	/**
-	 * @return mixed
-	 */
-	protected function get_transient() {
-		if ( is_multisite() ) {
-			return call_user_func_array( 'get_site_transient', func_get_args() );
-		}
-
-		return call_user_func_array( 'get_transient', func_get_args() );
-
-	}
-
-	/**
-	 * @return mixed
-	 */
-	protected function delete_transient() {
-		if ( is_multisite() ) {
-			return call_user_func_array( 'delete_site_transient', func_get_args() );
-		}
-
-		return call_user_func_array( 'delete_transient', func_get_args() );
-
+		return $this->store->delete_cache_branch( $path );
 	}
 
 	public function delete_cache_leaf( $path = [] ) {
-		if ( is_array( $path ) ) {
-			if ( ! empty( $path ) ) {
-				$path = $this->set_path_defaults( $path );
-				$path = CriticalCSS::TRANSIENT_PREFIX . implode( '_', $path );
-
-				return $this->delete_transient( $path );
-			}
-
-			return false;
-		}
-
-		return $this->delete_transient( $path );
+		return $this->store->delete_cache_leaf( $path );
 	}
 
 	/**
@@ -132,9 +66,7 @@ class Manager extends CriticalCSS\ComponentAbstract {
 	 * @return mixed
 	 */
 	public function get_cache_fragment( $path ) {
-		$path = $this->set_path_defaults( $path );
-
-		return $this->get_transient( CriticalCSS::TRANSIENT_PREFIX . implode( '_', $path ) );
+		return $this->store->get_cache_fragment( $path );
 	}
 
 	/**
@@ -142,103 +74,7 @@ class Manager extends CriticalCSS\ComponentAbstract {
 	 * @param $value
 	 */
 	public function update_cache_fragment( $path, $value ) {
-		$path = $this->set_path_defaults( $path );
-		$this->build_cache_tree( array_slice( $path, 0, - 1 ) );
-
-		return $this->update_tree_leaf( $path, $value );
-	}
-
-	/**
-	 * @param $path
-	 */
-	protected function build_cache_tree( $path ) {
-		$levels = count( $path );
-		$expire = $this->get_expire_period();
-		for ( $i = 0; $i < $levels; $i ++ ) {
-			$transient_id       = CriticalCSS::TRANSIENT_PREFIX . implode( '_', array_slice( $path, 0, $i + 1 ) );
-			$transient_cache_id = $transient_id;
-			if ( 'cache' != $path[ $i ] ) {
-				$transient_cache_id .= '_cache';
-			}
-			$transient_cache_id .= '_1';
-			$cache              = $this->get_transient( $transient_cache_id );
-			$transient_value    = [];
-			if ( $i + 1 < $levels ) {
-				$transient_value[] = CriticalCSS::TRANSIENT_PREFIX . implode( '_', array_slice( $path, 0, $i + 2 ) );
-			}
-			if ( ! is_null( $cache ) && false !== $cache ) {
-				$transient_value = array_unique( array_merge( $cache, $transient_value ) );
-			}
-			$this->set_transient( $transient_cache_id, $transient_value, $expire );
-			$transient_counter_id = $transient_id;
-			if ( 'cache' != $path[ $i ] ) {
-				$transient_counter_id .= '_cache';
-			}
-			$transient_counter_id .= '_count';
-			$transient_counter    = $this->get_transient( $transient_counter_id );
-			if ( is_null( $transient_counter ) || false === $transient_counter ) {
-				$this->set_transient( $transient_counter_id, 1, $expire );
-			}
-		}
-	}
-
-	/**
-	 * @return int
-	 */
-	public function get_expire_period() {
-		return apply_filters( 'wp_criticalcss_cache_expire_period', absint( $this->settings['web_check_interval'] ) );
-	}
-
-	/**
-	 *
-	 */
-	protected function set_transient() {
-		if ( is_multisite() ) {
-			return call_user_func_array( 'set_site_transient', func_get_args() );
-		}
-
-		return call_user_func_array( 'set_transient', func_get_args() );
-
-	}
-
-	/**
-	 * @param $path
-	 * @param $value
-	 */
-	protected function update_tree_leaf( $path, $value ) {
-
-		$leaf              = CriticalCSS::TRANSIENT_PREFIX . implode( '_', $path );
-		$parent_path       = array_slice( $path, 0, is_multisite() ? - 2 : - 1 );
-		$parent            = CriticalCSS::TRANSIENT_PREFIX . implode( '_', $parent_path );
-		$counter_transient = $parent;
-		$cache_transient   = $parent;
-		if ( 'cache' != end( $parent_path ) ) {
-			$counter_transient .= '_cache';
-			$cache_transient   .= '_cache';
-		}
-		$counter_transient .= '_count';
-		$counter           = (int) $this->get_transient( $counter_transient );
-		$cache_transient   .= "_{$counter}";
-		$cache             = $this->get_transient( $cache_transient );
-		$count             = count( $cache );
-		$cache_keys        = array_flip( $cache );
-		$expire            = $this->get_expire_period();
-		if ( ! isset( $cache_keys[ $leaf ] ) ) {
-			if ( $count >= apply_filters( 'rocket_async_css_max_branch_length', 50 ) ) {
-				$counter ++;
-				$this->set_transient( $counter_transient, $counter, $expire );
-				$cache_transient = $parent;
-				if ( 'cache' != end( $parent_path ) ) {
-					$cache_transient .= '_cache';
-				}
-				$cache_transient .= "_{$counter}";
-				$cache           = [];
-			}
-			$cache[] = $leaf;
-			$this->set_transient( $cache_transient, $cache, $expire );
-		}
-
-		return $this->set_transient( $leaf, $value, $expire );
+		return $this->store->update_cache_fragment( $path, $value );
 	}
 
 	/**
@@ -256,7 +92,7 @@ class Manager extends CriticalCSS\ComponentAbstract {
 	 *
 	 */
 	public function reset_web_check_transients() {
-		$this->delete_cache_branch();
+		$this->store->delete_cache_branch();
 	}
 
 	/**
@@ -274,7 +110,7 @@ class Manager extends CriticalCSS\ComponentAbstract {
 				'type'      => 'post',
 			]
 		);
-		$this->delete_cache_branch( [ $hash ] );
+		$this->store->delete_cache_branch( [ $hash ] );
 	}
 
 	/**
@@ -290,7 +126,7 @@ class Manager extends CriticalCSS\ComponentAbstract {
 				'type'      => 'term',
 			]
 		);
-		$this->delete_cache_branch( [ $hash ] );
+		$this->store->delete_cache_branch( [ $hash ] );
 	}
 
 	/**
@@ -328,6 +164,6 @@ class Manager extends CriticalCSS\ComponentAbstract {
 				]
 			);
 		}
-		$this->delete_cache_branch( [ $hash ] );
+		$this->store->delete_cache_branch( [ $hash ] );
 	}
 }
--- tests/WP/CriticalCSS/Testing/Integration/CriticalCSS/Cache/ManagerTest.php
+++ tests/WP/CriticalCSS/Testing/Integration/CriticalCSS/Cache/ManagerTest.php
@@ -6,67 +6,6 @@ use WP\CriticalCSS\Testing\Integration\TestCase;
 
 class ManagerTest extends TestCase {
 
-
-	public function test_update_cache_fragment() {
-		WPCCSS()->get_settings_manager()->update_settings(
-			[
-				'template_cache'     => 'off',
-				'web_check_interval' => HOUR_IN_SECONDS,
-			]
-		);
-		WPCCSS()->init();
-		$this->assertTrue( WPCCSS()->get_cache_manager()->update_cache_fragment( [ 'test' ], true ) );
-		$this->assertTrue( WPCCSS()->get_cache_manager()->get_cache_fragment( [ 'test' ] ) );
-	}
-
-	public function test_delete_cache_branch() {
-		WPCCSS()->get_settings_manager()->update_settings(
-			[
-				'template_cache'     => 'off',
-				'web_check_interval' => HOUR_IN_SECONDS,
-			]
-		);
-		WPCCSS()->init();
-		WPCCSS()->get_cache_manager()->update_cache_fragment( [ 'test' ], true );
-		$this->assertTrue( WPCCSS()->get_cache_manager()->delete_cache_branch() );
-		$this->assertFalse( WPCCSS()->get_cache_manager()->get_cache_fragment( [] ) );
-	}
-
-	public function test_delete_cache_leaf() {
-		WPCCSS()->get_settings_manager()->update_settings(
-			[
-				'template_cache'     => 'off',
-				'web_check_interval' => HOUR_IN_SECONDS,
-			]
-		);
-		WPCCSS()->init();
-		WPCCSS()->get_cache_manager()->update_cache_fragment( [ 'test' ], true );
-		$this->assertTrue( WPCCSS()->get_cache_manager()->delete_cache_leaf( [ 'test' ] ) );
-		$this->assertFalse( WPCCSS()->get_cache_manager()->get_cache_fragment( [ 'test' ] ) );
-	}
-
-	public function test_get_cache_fragment() {
-		WPCCSS()->get_settings_manager()->update_settings(
-			[
-				'template_cache'     => 'off',
-				'web_check_interval' => HOUR_IN_SECONDS,
-			]
-		);
-		WPCCSS()->init();
-		WPCCSS()->get_cache_manager()->update_cache_fragment( [ 'test' ], true );
-		$this->assertTrue( WPCCSS()->get_cache_manager()->get_cache_fragment( [ 'test' ] ) );
-	}
-
-	public function test_get_expire_period() {
-		WPCCSS()->get_settings_manager()->update_settings(
-			[
-				'web_check_interval' => HOUR_IN_SECONDS,
-			]
-		);
-		WPCCSS()->init();
-		$this->assertEquals( HOUR_IN_SECONDS, WPCCSS()->get_cache_manager()->get_expire_period() );
-	}
-
 	public function test_init_template_cache_off() {
 		WPCCSS()->init();
 		$this->assertEquals(
