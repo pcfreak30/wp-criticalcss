8c5a9c9a2fda90cb0c69bc4e83130fc7a7413112
Derrick Hammer <derrick@derrickhammer.com>
2017-07-10 20:28:38 -0400

    *Add defaults to mock

--- tests/WP/CriticalCSS/Testing/Unit/CriticalCSS/Settings/ManagerMock.php
+++ tests/WP/CriticalCSS/Testing/Unit/CriticalCSS/Settings/ManagerMock.php
@@ -6,7 +6,14 @@ use WP\CriticalCSS\Settings\Manager;
 
 class ManagerMock extends Manager {
 	public function get_settings() {
-		return WPCCSS()->get_settings();
+		$defaults = [];
+		foreach ( $this->settings as $setting ) {
+			$defaults[ $setting ] = null;
+		}
+
+		$settings = array_merge( $this->get_defaults(), WPCCSS()->get_settings() );
+
+		return $settings;
 	}
 
 	public function update_settings( array $settings ) {
