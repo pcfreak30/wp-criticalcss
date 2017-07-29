5b7a69c1cc5c8fa94a09265d8d23954cd7583cfb
Derrick Hammer <derrick@derrickhammer.com>
2017-07-29 16:30:28 -0400

    *Enable requests to not require authentication

--- lib/WP/CriticalCSS/API.php
+++ lib/WP/CriticalCSS/API.php
@@ -12,6 +12,7 @@ class API extends ComponentAbstract {
 	const STATUS_QUEUED = 'JOB_QUEUED';
 	const STATUS_ONGOING = 'JOB_ONGOING';
 	const STATUS_DONE = 'JOB_DONE';
+	const BASE_URL = 'https://criticalcss.com/api/';
 	/**
 	 * @var
 	 */
@@ -39,25 +40,33 @@ class API extends ComponentAbstract {
 	/**
 	 * @param string $type
 	 * @param string $endpoint
+	 * @param bool   $auth
 	 * @param array  $query_args
 	 * @param array  $args
 	 *
 	 * @return array|bool|mixed|object
 	 */
-	private function _send_request( $type, $endpoint, $query_args = [], $args = [] ) {
+	private function _send_request( $type, $endpoint, $auth = true, $query_args = [], $args = [] ) {
 		$type = strtolower( $type );
 		$func = "wp_remote_{$type}";
 		if ( ! function_exists( $func ) ) {
 			return false;
 		}
+		$api_url    = self::BASE_URL;
 		$query_args = array_merge( $query_args, [
 			'version' => CriticalCSS::VERSION,
 		] );
-		$response   = $func( add_query_arg( $query_args, "https://criticalcss.com/api/premium/{$endpoint}" ), array_merge_recursive( [
-			'headers' => [
-				'Authorization' => 'JWT ' . $this->api_key,
-			],
-		], $args ) );
+		$auth_args  = [];
+		if ( $auth ) {
+			$auth_args = [
+				'headers' => [
+					'Authorization' => 'JWT ' . $this->api_key,
+				],
+			];
+			$api_url   .= 'premium/';
+		}
+		$api_url  .= $endpoint;
+		$response = $func( add_query_arg( $query_args, $api_url ), array_merge_recursive( $auth_args, $args ) );
 		if ( $response instanceof \WP_Error ) {
 			return $response;
 		}
@@ -74,7 +83,7 @@ class API extends ComponentAbstract {
 	 * @return array|bool|mixed|object
 	 */
 	public function get_result( $item_id ) {
-		return $this->_send_request( 'get', 'results', [
+		return $this->_send_request( 'get', 'results', true, [
 			'resultId' => $item_id,
 		] );
 	}
@@ -85,7 +94,7 @@ class API extends ComponentAbstract {
 	 * @return array|bool|mixed|object
 	 */
 	public function generate( array $item ) {
-		$response = $this->_send_request( 'post', 'generate', [], [
+		$response = $this->_send_request( 'post', 'generate', true, [], [
 			'body' => [
 				'height'                   => 900,
 				'width'                    => 1300,
@@ -105,7 +114,7 @@ class API extends ComponentAbstract {
 		$cache      = $this->app->get_cache_manager()->get_store()->get_transient( $cache_name );
 
 		if ( empty( $cache ) ) {
-			$response = $this->_send_request( 'get', 'invalid-generate-url-rules' );
+			$response = $this->_send_request( 'get', 'invalid-generate-url-rules', false );
 			$cache    = $response->rules;
 			$this->app->get_cache_manager()->get_store()->set_transient( $cache_name, $cache, WEEK_IN_SECONDS * 2 );
 		}
