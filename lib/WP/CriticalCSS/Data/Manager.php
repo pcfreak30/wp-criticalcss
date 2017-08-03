64dfa687540dc75bc01fa36e040bd67925f4a120
Derrick Hammer <derrick@derrickhammer.com>
2017-08-03 16:18:22 -0400

    *Use cache manager on template cache

--- lib/WP/CriticalCSS/Data/Manager.php
+++ lib/WP/CriticalCSS/Data/Manager.php
@@ -32,8 +32,8 @@ class Manager extends CriticalCSS\ComponentAbstract {
 			$item = WPCCSS()->get_request()->get_current_page_type();
 		}
 		if ( 'on' === $this->settings['template_cache'] && ! empty( $item['template'] ) ) {
-			$name  = "criticalcss_{$name}_" . md5( $item['template'] );
-			$value = get_transient( $name );
+			$name  = [ $name, md5( $item['template'] ) ];
+			$value = $this->app->get_cache_manager()->get_store()->get_cache_fragment( $name );
 		} else {
 			if ( 'url' === $item['type'] ) {
 				$name  = [ $name, md5( $item['url'] ) ];
@@ -77,7 +77,8 @@ class Manager extends CriticalCSS\ComponentAbstract {
 	 */
 	public function set_item_data( $item, $name, $value, $expires = 0 ) {
 		if ( 'on' === $this->settings['template_cache'] && ! empty( $item['template'] ) ) {
-			$name = "criticalcss_{$name}_" . md5( $item['template'] );
+			$name = [ $name, md5( $item['template'] ) ];
+			$this->app->get_cache_manager()->get_store()->update_cache_fragment( $name, $value );
 			set_transient( $name, $value, $expires );
 		} else {
 			if ( 'url' === $item['type'] ) {
