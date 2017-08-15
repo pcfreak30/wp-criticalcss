97f87583dd8ae0e2cb26e6d789153175308b708e
Derrick Hammer <derrick@derrickhammer.com>
2017-08-15 10:58:47 -0400

    *Run log components create_table instead of create table in installer

--- lib/WP/CriticalCSS/Installer.php
+++ lib/WP/CriticalCSS/Installer.php
@@ -5,6 +5,12 @@ namespace WP\CriticalCSS;
 
 use pcfreak30\WordPress\Plugin\Framework\ComponentAbstract;
 
+/**
+ * Class Installer
+ *
+ * @package WP\CriticalCSS
+ * @property \WP\CriticalCSS $plugin
+ */
 class Installer extends ComponentAbstract {
 
 	/**
@@ -14,6 +20,9 @@ class Installer extends ComponentAbstract {
 
 	}
 
+	/**
+	 *
+	 */
 	public function activate() {
 		$wpdb        = $this->wpdb;
 		$settings    = $this->plugin->settings_manager->get_settings();
@@ -78,35 +87,14 @@ class Installer extends ComponentAbstract {
 
 		$this->plugin->web_check_queue->create_table();
 		$this->plugin->api_queue->create_table();
-		$this->create_processed_table();
+		$this->plugin->log->create_table();
 
 		flush_rewrite_rules();
 	}
 
-	private function create_processed_table() {
-		$wpdb = $this->wpdb;
-		if ( ! function_exists( 'dbDelta' ) ) {
-			include_once ABSPATH . 'wp-admin/includes/upgrade.php';
-		}
-
-		$charset_collate = $wpdb->get_charset_collate();
-		if ( is_multisite() ) {
-			$table = "{$wpdb->base_prefix}{$this->plugin->get_safe_slug()}_processed_items";
-		} else {
-			$table = "{$wpdb->prefix}{$this->plugin->get_safe_slug()}";
-		}
-		$sql = "CREATE TABLE $table (
-  id MEDIUMINT(9) NOT NULL AUTO_INCREMENT,
-  template  VARCHAR(255),
-  object_id  BIGINT(10),
-  type VARCHAR (10),
-  url TEXT,";
-		if ( is_multisite() ) {
-			$sql .= "\n" . 'blog_id BIGINT(20),';
-		}
-		dbDelta( "$sql\nPRIMARY KEY  (id)\n) {$charset_collate};" );
-	}
-
+	/**
+	 *
+	 */
 	public function deactivate() {
 		flush_rewrite_rules();
 	}
