f7fae92598eb8b9d4ff29a36d791fc2af3c5d85b
Derrick Hammer <derrick@derrickhammer.com>
2017-08-15 10:03:00 -0400

    *Add stricter type checks

--- lib/WP/CriticalCSS/Web/Check/Background/Process.php
+++ lib/WP/CriticalCSS/Web/Check/Background/Process.php
@@ -73,7 +73,7 @@ class Process extends ProcessAbstract {
 				if ( 'stylesheet' == $rel ) {
 
 					// If not a stylesheet, rocket_async_css_process_file return false, or exclude regex/file extension matches, move on
-					if ( 'stylesheet' != $rel ) {
+					if ( 'stylesheet' !== $rel ) {
 						continue;
 					}
 					if ( 0 === strpos( $href, '//' ) ) {
@@ -137,7 +137,7 @@ class Process extends ProcessAbstract {
 
 		if ( ! $changed ) {
 			$body = $document->getElementsByTagName( 'body' )->item( 0 );
-			if ( ! empty( $body ) ) {
+			if ( null !== $body ) {
 				$new_html_hash = hash( "crc32b", $document->saveHTML( $body ) );
 				if ( empty( $html_hash ) || $html_hash != $new_html_hash ) {
 					$changed   = true;
