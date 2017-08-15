d62dbf58f14d2b9d03890b593b296d11552df94a
Derrick Hammer <derrick@derrickhammer.com>
2017-08-14 20:04:41 -0400

    *Update phpdoc

--- tests/WP/CriticalCSS/Testing/Admin/UIMock.php
+++ tests/WP/CriticalCSS/Testing/Admin/UIMock.php
@@ -8,7 +8,7 @@ use WP\CriticalCSS\Admin\UI;
 
 class UIMock extends UI {
 	/**
-	 * @return \WP\CriticalCSS\Queue\ListTable
+	 * @return \WP\CriticalCSS\Queue\ListTableAbstract
 	 */
 	public function get_queue_table() {
 		return $this->queue_table;
