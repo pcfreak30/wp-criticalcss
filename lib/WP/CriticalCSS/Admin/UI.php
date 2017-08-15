d9fb3cfbd40f28ecb228c596508ab239a68080a7
Derrick Hammer <derrick@derrickhammer.com>
2017-08-15 08:38:23 -0400

    *Add web_check_table to class *Clean up css and improve formatting *Delete dummy queue options after save to keep options table clean *Add descriptions at the top of the tables to help the end user understand what is going on

--- lib/WP/CriticalCSS/Admin/UI.php
+++ lib/WP/CriticalCSS/Admin/UI.php
@@ -4,7 +4,10 @@ namespace WP\CriticalCSS\Admin;
 
 use pcfreak30\WordPress\Plugin\Framework\ComponentAbstract;
 use WP\CriticalCSS;
-use WP\CriticalCSS\Queue\API\Table;
+use WP\CriticalCSS\API;
+use WP\CriticalCSS\Queue\API\Table as APITable;
+use WP\CriticalCSS\Queue\Web\Check\Table as WebCheckTable;
+use WP\CriticalCSS\Settings\API as SettingsAPI;
 
 /**
  * Class UI
@@ -23,18 +26,24 @@ class UI extends ComponentAbstract {
 	private $api_table;
 
 	private $api;
+	/**
+	 * @var \WP\CriticalCSS\Queue\Web\Check\Table
+	 */
+	private $web_check_table;
 
 	/**
 	 * UI constructor.
 	 *
-	 * @param \WP\CriticalCSS\Settings\API    $settings_ui
-	 * @param \WP\CriticalCSS\API             $api
-	 * @param \WP\CriticalCSS\Queue\API\Table $api_table
+	 * @param \WP\CriticalCSS\API|\WP\CriticalCSS\Settings\API $settings_ui
+	 * @param \WP\CriticalCSS\API                              $api
+	 * @param APITable                                         $api_table
+	 * @param WebCheckTable                                    $web_check_table
 	 */
-	public function __construct( \WP\CriticalCSS\Settings\API $settings_ui, CriticalCSS\API $api, Table $api_table ) {
-		$this->settings_ui = $settings_ui;
-		$this->api         = $api;
-		$this->api_table   = $api_table;
+	public function __construct( SettingsAPI $settings_ui, API $api, APITable $api_table, WebCheckTable $web_check_table ) {
+		$this->settings_ui     = $settings_ui;
+		$this->api             = $api;
+		$this->api_table       = $api_table;
+		$this->web_check_table = $web_check_table;
 	}
 
 
@@ -56,6 +65,14 @@ class UI extends ComponentAbstract {
 			add_action( 'pre_update_option_wp_criticalcss', [
 				$this,
 				'sync_options',
+			], 10, 2 );
+			add_action( 'update_option_wp_criticalcss_web_check_queue', [
+				$this,
+				'delete_dummy_option',
+			], 10, 2 );
+			add_action( 'update_option_wp_criticalcss_api_queue', [
+				$this,
+				'delete_dummy_option',
 			], 10, 2 );
 		}
 	}
@@ -81,7 +98,7 @@ class UI extends ComponentAbstract {
 			'screen_option',
 		] );
 		$this->settings_ui->add_section( [
-			'id'    => CriticalCSS::OPTIONNAME,
+			'id'    => $this->plugin->get_option_name(),
 			'title' => 'Options',
 		] );
 		$this->settings_ui->add_field( $this->plugin->get_option_name(), [
@@ -122,20 +139,61 @@ class UI extends ComponentAbstract {
 	 */
 	public function settings_ui() {
 		require_once ABSPATH . 'wp-admin/options-head.php';
+		$template_cache = 'on' === $this->plugin->settings_manager->get_setting( 'template_cache' );
+		if ( ! $template_cache ) {
+			$this->settings_ui->add_section( [
+				'id'    => 'wp_criticalcss_web_check_queue',
+				'title' => 'Web Check Queue',
+				'form'  => false,
+			] );
+		}
 		$this->settings_ui->add_section( [
-			'id'    => 'wp_criticalcss_queue',
+			'id'    => 'wp_criticalcss_api_queue',
 			'title' => 'API Queue',
 			'form'  => false,
 		] );
-
-		ob_start();
-
 		?>
 		<style type="text/css">
-			.queue > th {
+			.form-table .api_queue > th, .form-table .web_check_queue > th {
+				display: none;
+			}
+
+			.no-items, .manage-column, .form-table .api_queue td, .form-table .web_check_queue td {
+				text-align: center !important;
+			}
+
+			.form-table th {
+				width: auto;
+			}
+
+			.group h2 {
 				display: none;
 			}
 		</style>
+
+		<?php
+		if ( ! $template_cache ): ?>
+			<?php ob_start(); ?>
+			<p>
+				<?php _e( 'What is this? This queue is designed to process your content only if "template" mode is off. It detects changes to the content and sends to the "API Queue" if any are found.', $this->plugin->get_lang_domain() ); ?>
+			</p>
+			<form method="post">
+				<?php
+				$this->web_check_table->prepare_items();
+				$this->web_check_table->display();
+				?>
+			</form>
+		<?php endif;
+		$this->settings_ui->add_field( 'wp_criticalcss_web_check_queue', [
+			'name'  => 'web_check_queue',
+			'label' => null,
+			'type'  => 'html',
+			'desc'  => ob_get_clean(),
+		] );
+		ob_start(); ?>
+		<p>
+			<?php _e( 'What is this? This queue actually processes requests by sending them to CriticalCSS.com and waiting on them to process. When done it will purge any supported cache and make that page just a bit faster :)', $this->plugin->get_lang_domain() ); ?>
+		</p>
 		<form method="post">
 			<?php
 			$this->api_table->prepare_items();
@@ -143,12 +201,13 @@ class UI extends ComponentAbstract {
 			?>
 		</form>
 		<?php
-		$this->settings_ui->add_field( 'wp_criticalcss_queue', [
-			'name'  => 'queue',
+		$this->settings_ui->add_field( 'wp_criticalcss_api_queue', [
+			'name'  => 'api_queue',
 			'label' => null,
 			'type'  => 'html',
 			'desc'  => ob_get_clean(),
 		] );
+
 
 		$this->settings_ui->admin_init();
 		$this->settings_ui->show_navigation();
@@ -195,12 +254,8 @@ class UI extends ComponentAbstract {
 	 *
 	 */
 	public function screen_option() {
-		add_screen_option( 'per_page', [
-			'label'   => 'Queue Items',
-			'default' => 20,
-			'option'  => 'queue_items_per_page',
-		] );
 		$this->api_table->init();
+		$this->web_check_table->init();
 	}
 
 	/**
@@ -228,5 +283,14 @@ class UI extends ComponentAbstract {
 		}
 
 		return $value;
+	}
+
+	/**
+	 * @param        $old_value
+	 * @param        $value
+	 * @param string $option
+	 */
+	public function delete_dummy_option( $old_value, $value, $option ) {
+		delete_option( $option );
 	}
 }
