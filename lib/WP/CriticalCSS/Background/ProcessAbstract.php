7e5b1f3ff5420479eb3b033a4e6fa51f151cf6fd
Derrick Hammer <derrick@derrickhammer.com>
2019-07-13 06:59:44 -0400

    *Switch to using serialize to compare

--- lib/WP/CriticalCSS/Background/ProcessAbstract.php
+++ lib/WP/CriticalCSS/Background/ProcessAbstract.php
@@ -218,7 +218,7 @@ abstract class ProcessAbstract extends \WP_Background_Process {
 
 			// Update or delete current batch.
 			if ( ! empty( $batch->data ) ) {
-				if ( $original_batch != $batch ) {
+				if ( serialize( $original_batch ) !== serialize( $batch ) ) {
 					$this->update( $batch->key, $batch->data );
 					$original_batch = $batch;
 				}
